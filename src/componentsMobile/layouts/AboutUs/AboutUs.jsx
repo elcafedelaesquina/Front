101db87b367aa02c5d42bc6b7b9c5844cc8dd93f
Samuel Mejía <samuelmejiagir@gmail.com>
2023-05-04 13:27:35 -0500

    Subiendo algunos cambios

--- src/componentsMobile/layouts/AboutUs/AboutUs.jsx
+++ src/componentsMobile/layouts/AboutUs/AboutUs.jsx
@@ -9,7 +9,7 @@ const imgtest = 'https://res.cloudinary.com/dmrq9n2im/image/upload/v1680140690/E
     <div className={Style.aboutus}>
       <h2 className={Style.subtitle}>Nuestra historia</h2>
       <div className={Style.imgMain}>
-      <img className={Style.img} src={imgtest} alt="" />
+      <img className={Style.imgReference} src={imgtest} alt="" />
       </div>
       <p className={Style.txt}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est fugit odio sunt dignissimos, tenetur autem itaque nam vel facere, voluptatibus ullam, rem veniam qui totam corporis eos accusamus eum hic?</p>
     </div>
