24b32f09db127ef5353d04763fe27c26c433f73b
Samuel Mejía <samuelmejiagir@gmail.com>
2023-05-16 14:39:28 -0500

    some changes

--- src/componentsMobile/pages/Cafeteria/Cafeteria.jsx
+++ src/componentsMobile/pages/Cafeteria/Cafeteria.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Style from './cafeteria.module.css'
-
 import { NavBar } from "../../layouts/navbar/Navbar";
 import { Slider } from "../../layouts/slider/Slider";
 import { HomeCafeteria } from "../../layouts/homeCafeteria/HomeCafeteria";
@@ -11,7 +9,7 @@ import { FooterCafeteria } from "../../layouts/footercafeteria/FooterCafeteria";
 
 export const Cafeteria = () => {
   return (
-    <div className={Style.cafeteria}>
+    <div>
       <NavBar />
       <Slider />
       <HomeCafeteria />
