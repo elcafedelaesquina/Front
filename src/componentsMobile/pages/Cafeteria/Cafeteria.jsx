e078665629a0b40fc27c8349e7cc9d25948b7f16
Samuel Mejía <samuelmejiagir@gmail.com>
2023-05-19 17:07:08 -0500

    organizando los layouts de cafeteria en una sola carpeta

--- src/componentsMobile/pages/Cafeteria/Cafeteria.jsx
+++ src/componentsMobile/pages/Cafeteria/Cafeteria.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { NavBar } from "../../layouts/navbar/Navbar";
 import { Slider } from "../../layouts/slider/Slider";
-import { HomeCafeteria } from "../../layouts/homeCafeteria/HomeCafeteria";
-import { CategoriesProducts } from "../../layouts/CategoriesProducts/CategoriesProducts";
-import { FeaturedProducts } from "../../layouts/featuredproducts/FeaturedProducts";
-import { CarProducts } from "../../layouts/CarProducts/CarProducts";
-import { FooterCafeteria } from "../../layouts/footercafeteria/FooterCafeteria";
-import { Menu } from "../../layouts/Menu/Menu"
+import { HomeCafeteria } from "../../layouts/layoutsCafeteria/homeCafeteria/HomeCafeteria";
+import { CategoriesProducts } from "../../layouts/layoutsCafeteria/CategoriesProducts/CategoriesProducts";
+import { FeaturedProducts } from "../../layouts/layoutsCafeteria/featuredproducts/FeaturedProducts";
+import { CarProducts } from "../../layouts/layoutsCafeteria/CarProducts/CarProducts";
+import { FooterCafeteria } from "../../layouts/layoutsCafeteria/footercafeteria/FooterCafeteria";
+import { Menu } from "../../layouts/layoutsCafeteria/Menu/Menu"
 
 export const Cafeteria = () => {
   return (
