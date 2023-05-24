5bd0cdb80db81d2827a5599d866368e8da0c7130
JUANKAMILO2005 <ojedavalenciajuancamilo@gmail.com>
2023-05-24 00:24:23 -0500

    Se agrega fincas

--- src/components/pages/finca/index.js
+++ src/components/pages/finca/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Header } from "../../layouts/header/header"
 import { HeaderFinca } from "./headerFinca"
 import { FooterFinca } from "./footerFinca"
+import { HomeFinca } from "./pagesFinca/Home"
 
 const Finca = () => {
   return(
     <>
     <Header></Header>
     <HeaderFinca></HeaderFinca>
+    <HomeFinca></HomeFinca>
     <FooterFinca></FooterFinca>
     </>
   )
