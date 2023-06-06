0248750eca99eed497b3d8b73af42cabaf001b35
cristofertOcampo <ocampocristofert@gmail.com>
2023-06-06 12:19:34 -0500

    adminCafeteria

--- src/componentsMobile/layouts/layoutsAdminCafeterias/GridProductsAdmin/GridProductsAdmin.jsx
+++ src/componentsMobile/layouts/layoutsAdminCafeterias/GridProductsAdmin/GridProductsAdmin.jsx
@@ -1,34 +1,22 @@
-import React, {useState, useEffect} from 'react'
-import Style from './gridProductsAdmin.module.css'
+import React, { useState, useEffect } from "react";
+import Style from "./gridProductsAdmin.module.css";
 
-export const GridProductsAdmin = () => {
-
-    const [data, setData] = useState([]);
-
-    const getProductsCafeterias = async () => {
-        await fetch("https://apimainejetravel.azurewebsites.net/api/Product/Lista/2")
-          .then((response) => response.json())
-          .then((data) => {
-            const { list } = data;
-            setData(list[0]);
-          })
-          .catch((error) => {
-            // Manejar el error
-            console.error(error);
-          });
-      };
-    
-      useEffect(() => {
-        getProductsCafeterias();
-      }, []);
-
-      const valores = data.reverse();
-
+export const GridProductsAdmin = ({valores, cardId, setIsVisible, isVisible }) => {
   return (
     <div className={Style.gridContainer}>
-        {valores.map((item) => (
-            <div className={Style.product} key={item.cod_product}><img src={item.image} alt="image product" className={Style.image} /> {item.name}</div>
-        ))}
+      {valores.map((item) => (
+        <div
+          className={Style.product}
+          key={item.cod_product}
+          onClick={() => {
+            setIsVisible(!isVisible)
+            cardId(item)
+          }}
+        >
+          <img src={item.image} alt="image product" className={Style.image} />{" "}
+          {item.name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
